4b1e7e0e8546c86f44df289894d58fcb97372bfb
josephrmoore <josephrmoore@gmail.com>
2024-01-08 22:27:10 -0800

    images, other fixes for bosses & items

--- src/App.js
+++ src/App.js
@@ -2,21 +2,31 @@ import './App.css';
 import React from 'react';
 import data from "./zones";
 
+	var useItems = true;
+	var useBosses = true;
+	var useLabels = true;
 
 function App() {
-  return (
-    <div className="app">
- 		<canvas id="canvas"></canvas>
- 		<Map />
-    </div>
-  );
+	
+	var labelText = "ON"
+	
+	return (
+    	<div className="app">
+ 			<canvas id="canvas"></canvas>
+ 			<Map />
+ 			<div className="control_panel">
+ 				<button id="toggleText" className="toggletext" onClick={(e) => toggleLabels(e, useLabels)} >Area Name</button>
+ 				<button id="toggleItems" className="toggleitems" onClick={(e) => toggleItems(e, useItems)} >Item Count</button>
+ 				<button id="toggleBosses" className="togglebosses" onClick={(e) => toggleBosses(e, useBosses)} >Bosses</button>
+ 			</div>
+ 		</div>
+ 	);
 }
 
 class Map extends React.Component {
 	constructor(props) {
 		super(props);
 		// view: mapview, zoneview
-		// 
 		this.state = {
 			view : "mapview",
 			isPortalActive : false,
@@ -39,15 +49,12 @@ class Map extends React.Component {
 	}
 	
 	render(){
-//		localStorage.setItem('connections', "");
 		var newstyle = this.props.color;
-// 		console.log(newstyle);
 		return (
-    <div className={"map "+this.state.view} onClick={(e)=> mapView(this, "map")} onContextMenu={(e)=> mapRightClick(this, "map")}>
+    <div id="map" className={"map "+this.state.view} onClick={(e)=> mapView(this, "map")} onContextMenu={(e)=> mapRightClick(this, "map")} data-mode={this.state.view} data-mainzone={this.state.main_zone}>
  		{this.zones.map((datum, key) => {
 			return <Zone key={key} id={datum.id} name={datum.name} color={datum.color} portals={datum.portals} state={this.state} mapstatechange={this.mapstatechange} />
 		})}
-		<button id="toggleText" className="toggletext" onClick={(e) => toggleLabels(e, this)} >Toggle Area Labels</button>
     </div>
 		);
 	}
@@ -60,24 +67,18 @@ class Zone extends React.Component {
 	}
 
 	render(){
-//		var newstyle = this.props.color;
 		var labelClass = "";
 		var zoneState = "";
 		
 		if(this.props.id === this.props.state.main_zone){
 			zoneState = " main";
 		} 
-//			console.log(this.props.state.side_zones);
 		try{
-		for(var i=0; i<this.props.state.side_zones.length; i++){
-//			console.log(this.props.state.side_zones[i]);
-//			console.log(this.props.id);
-			if(this.props.id === this.props.state.side_zones[i]){
-//				console.log("WOOP!");
-				zoneState = " side";
+			for(var i=0; i<this.props.state.side_zones.length; i++){
+				if(this.props.id === this.props.state.side_zones[i]){
+					zoneState = " side";
+				}
 			}
-//			console.log(zoneState);
-		}
 		} catch(err) {
 			console.log("No connections yet");
 		}
@@ -86,13 +87,16 @@ class Zone extends React.Component {
 		if(!this.props.state.labels){
 			labelClass = " hidelabels"
 		}
-//		console.log(zoneState);
+		
 		return (
 			<div className={"zone zone"+this.props.id+zoneState} data-id={this.props.id} data-name={this.props.name} data-color={this.props.color} onClick={(e)=> zoneClick(e, this.props.id, this)} onContextMenu={(e)=> zoneRightClick(e.target, this)}>
 				<h2 className={"label"+labelClass}>{this.props.name}</h2>
 				{this.props.portals.map((datum, key) => {
-					return <Portal key={key} id={datum.id} color={this.props.color} zoneid={this.props.id} state={this.props.state} mapstatechange={this.props.mapstatechange} />
+					return <Portal key={key} id={datum.id} color={this.props.color} zoneid={this.props.id} state={this.props.state} mapstatechange={this.props.mapstatechange} direction={datum.dir} name={datum.name} />
 				})}
+				<Clickbox type="items" />
+				{(this.props.id === 3 || this.props.id === 5 || this.props.id === 7 || this.props.id === 8) ? (<Clickbox type="bosses" />) : ("")}
+				
 	      	</div>
 		);
 	}
@@ -103,7 +107,8 @@ class Portal extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			connected : "unassigned"
+			connected : "unassigned",
+			partner_color : ""
 		};
 		this.portalstatechange = this.portalstatechange.bind(this);	
 	}
@@ -114,16 +119,107 @@ class Portal extends React.Component {
 	
 	render(){
 		return (
-			<div className={"portal portal"+this.props.id+" "+ this.state.connected} data-id={this.props.id} data-zoneid={this.props.zoneid} onClick={(e)=> clickPortal(this, e, "left")} onContextMenu={(e)=> clickPortal(this, e, "right")}></div>
+			<div className={"portal portal"+this.props.id+" "+ this.state.connected} data-id={this.props.id} data-zoneid={this.props.zoneid} onClick={(e)=> clickPortal(this, e, "left")} onContextMenu={(e)=> clickPortal(this, e, "right")} data-partnercolor={this.state.partner_color}><p className="portal_name">{this.props.name}</p></div>
 	  	);
   	}
+}
+
+class Clickbox extends React.Component {
+	
+	constructor(props) {
+		super(props);
+		this.state = {
+			// type: items, bosses
+			type : this.props.type,
+			value: 0,
+			boss: "unknown"
+		};
+		this.clickboxstatechange = this.clickboxstatechange.bind(this);	
+	}
+	
+	clickboxstatechange(obj){
+		this.setState(obj);
+	}
+	
+	render(){
+		return (
+			<div className={"clickbox "+this.state.type+" "+((this.state.boss)?this.state.boss:"")} onClick={(e)=> clickClickbox(e, this, "left")} onContextMenu={(e)=> clickClickbox(e, this, "right")}><span className={"value"}>{(this.state.type === "items") ? this.state.value : ""}</span></div>
+	  	);
+  	}
+}
+
+
+function clickClickbox(e, comp, dir){
+	var all_bosses = ["kraid", "phantoon", "draygon", "ridley", "unknown"];
+	e.preventDefault();
+	e.stopPropagation();
+	var maxvalue;
+	if(comp.state.type === "bosses"){
+		maxvalue = all_bosses.length-1;
+		if(dir === "left"){
+			// left click
+			if(comp.state.value < maxvalue){
+				comp.clickboxstatechange({
+					value : (Number(comp.state.value)+1),
+					boss : all_bosses[(Number(comp.state.value)+1)]
+				});
+			} else {
+				comp.clickboxstatechange({
+					value : 0,
+					boss : all_bosses[0]
+				});
+			}
+		} else if (dir === "right"){
+			// right click
+			if(comp.state.value > 0){
+				comp.clickboxstatechange({
+					value : (Number(comp.state.value)-1),
+					boss : all_bosses[(Number(comp.state.value)-1)]
+				});
+			} else {
+				comp.clickboxstatechange({
+					value : maxvalue,
+					boss : all_bosses[maxvalue]
+				});
+			}
+		}
+	} else if(comp.state.type === "items"){
+		maxvalue = 9;
+		if(!comp.state.value){
+			comp.clickboxstatechange({
+				value : 0
+			});
+		}
+		if(dir === "left"){
+			// left click
+			if(comp.state.value < maxvalue){
+				comp.clickboxstatechange({
+					value : (Number(comp.state.value)+1)
+				});
+			} else {
+				comp.clickboxstatechange({
+					value : 0
+				});
+			}
+		} else if (dir === "right"){
+			// right click
+			if(comp.state.value > 0){
+				comp.clickboxstatechange({
+					value : (Number(comp.state.value)-1)
+				});
+			} else {
+				comp.clickboxstatechange({
+					value : maxvalue
+				});
+			}
+		}
+	}
 }
 
 // MAP LOGIC
 
 
 function mapView(comp, fromWhich){
-//	console.log(comp);
 	if(fromWhich == "map"){
 		try {
 			comp.mapstatechange({
@@ -165,65 +261,29 @@ function zoneClick(e, id, comp){
 	var connections = localStorage.getItem("connections");
 	var cnxzones = [];
 	var filtered_cnxzones;
-//	var sidecounter = 1;
-//	var newarr = comp.props.state.side_zones;
-//	console.log(connections);
 	let cnx;
-	try {
-		cnx = JSON.parse(connections);
-		if(cnx.length>0){
-			for(var i=0; i<cnx.length; i++){
-				if(cnx[i][0][0] === comp.props.id || cnx[i][1][0] === comp.props.id){
-					for(var j=0; j<cnx[i].length; j++){
-						cnxzones.push(cnx[i][j][0]);
-						filtered_cnxzones = removeDuplicates(cnxzones);
-					}				
+	if(comp.props.state.view === "mapview"){
+		try {
+			cnx = JSON.parse(connections);
+			if(cnx.length>0){
+				for(var i=0; i<cnx.length; i++){
+					if(cnx[i][0][0] === comp.props.id || cnx[i][1][0] === comp.props.id){
+						for(var j=0; j<cnx[i].length; j++){
+							cnxzones.push(cnx[i][j][0]);
+							filtered_cnxzones = removeDuplicates(cnxzones);
+						}				
+					}
+				}			
+				if(filtered_cnxzones.length>0){
+					var index = filtered_cnxzones.indexOf(comp.props.id);
+					filtered_cnxzones.splice(index, 1);
 				}
 			}
-			
-//			console.log(filtered_cnxzones);
-			
-			if(filtered_cnxzones.length>0){
-				var index = filtered_cnxzones.indexOf(comp.props.id);
-				filtered_cnxzones.splice(index, 1);
-			}
-//			console.log(filtered_cnxzones);
-
+		} catch (err) {
+			console.log("local storage connections data not valid JSON");
 		}
-	} catch (err) {
-		console.log("local storage connections data not valid JSON");
+		zoneView(comp, filtered_cnxzones);
 	}
-//	console.log(filtered_cnxzones);
-	zoneView(comp, filtered_cnxzones);
-
-//	sidecounter = 1;
-//	console.log("in func");
-//	try{
-//		for(var k=0; k<filtered_cnxzones.length; k++){
-			// add sidezone class
-//				console.log("in k loop");
-	
-//			if(filtered_cnxzones[k] != id){
-//				console.log(comp);
-//				console.log(id);
-				
-//				newarr.push(filtered_cnxzones[k]);				
-				
-/*
-				var sideclass = 'sidezone'+(sidecounter);
-				sidecounter++;
-//				console.log(sideclass);
-//				console.log(all_zones[filtered_cnxzones[k]].classList);
-				all_zones[filtered_cnxzones[k]].classList.add(sideclass);
-//				console.log(all_zones[filtered_cnxzones[k]].classList);
-*/
-//			}
-//		}
-//		console.log(newarr);
-//	} catch (err) {
-//		console.log("nothing in connex array yet");
-//	}
-//	console.log(newarr);
 }
 
 function zoneRightClick(comp){
@@ -233,22 +293,6 @@ function zoneRightClick(comp){
 
 function zoneView(comp, side_ids){
 
-	console.log(comp);
-	console.log(side_ids);
-
-// if it's a MAP click
-/*
-	try {
-		comp.mapstatechange({
-			view : "zoneview"
-		});
-	} catch(err) {
-		console.log("zoneview: not a map");
-	}
-*/
-	
-// if it's a ZONE click
-//console.log(side_ids);
 	try {
 		comp.props.mapstatechange({
 			view : "zoneview",
@@ -264,39 +308,33 @@ function zoneView(comp, side_ids){
 // PORTAL LOGIC
 
 function clickPortal(comp, e, type){
+//	e.preventDefault();
 	e.stopPropagation();
-	var hasConnection = false;
-	for(var i=0; i<e.target.classList.length; i++){
-		if(e.target.classList[i] == "connected"){
-			hasConnection = true;
-		}
-	}
-//	console.log(hasConnection);
 	if(type == "left"){
-		if(!hasConnection){
+//		console.log(comp.state.connected);
+		if(comp.state.connected !== "connected"){
 			//left click
 			if(comp.props.state.isPortalActive){
 				// There is already a clicked portal
 				if(comp.props.state.active_portal[0] == comp.props.zoneid && comp.props.state.active_portal[1] == comp.props.id){
-//					console.log("same portal");
 					comp.props.mapstatechange({
 						isPortalActive : false,
 						active_portal : [null, null]
 					});	
 				} else {
 					makePartnership(comp, [comp.props.state.active_portal[0], comp.props.state.active_portal[1]], [comp.props.zoneid, comp.props.id]);
-//					console.log("make partnership");
 					comp.props.mapstatechange({
 						isPortalActive : false,
 						active_portal : [null, null]
 					});
 					comp.portalstatechange({
-						connected : "connected"
+						connected : "connected",
+						partner_color : data.zones[comp.props.state.active_portal[0]].color
 					});
+//					console.log(data.zones[comp.props.zoneid].color);
 				}
 
 			} else {
-	//			console.log(comp);
 				// There are no clicked portals
 				comp.props.mapstatechange({
 					isPortalActive : true,
@@ -305,10 +343,8 @@ function clickPortal(comp, e, type){
 				comp.portalstatechange({
 					connected : "connected"
 				});
-
+//				console.log(data.zones[comp.props.zoneid].color);
 			}
-//			console.log("TYPE FOR CONNX:");
-//			console.log(e.target.classList);
 		}
 	} else if (type == "right") {
 		// right click
@@ -318,7 +354,8 @@ function clickPortal(comp, e, type){
 				active_portal : [null, null]
 			});
 			comp.portalstatechange({
-				connected : "unassigned"
+				connected : "unassigned",
+				partner_color : ""
 			});
 		}
 	}
@@ -330,15 +367,11 @@ function clickPortal(comp, e, type){
 
 
 function makePartnership(comp, portal1, portal2){
-//	console.log(portal1);
-//	console.log(portal2);
-//	console.log(comp.props.state.connections);
 	var connections_exist = false;
 	var newportals = [portal1, portal2];
 	let existing_connections;
 	try {
 		existing_connections = JSON.parse(comp.props.state.connections);
-//		console.log(existing_connections);
 		connections_exist = true;
 	} catch (err) {
 		console.log("map state connections data not valid JSON");
@@ -359,20 +392,20 @@ function makePartnership(comp, portal1, portal2){
 	
 	drawLines();
 	
-//	console.log(comp.props.state.connections);
 }
 
 // DRAWING LINES
 
 function drawLines(){
+	clearLines();
+	var map = document.getElementById("map");
+	var map_mode = map.dataset.mode;
+	var map_main_zone = Number(map.dataset.mainzone);
 	var connections = localStorage.getItem("connections");
 	var all_portals = document.getElementsByClassName("portal");
 	var all_zones = document.getElementsByClassName("zone");
 	var x, y;
 	var color1, color2, c1, c2 = "white";
-	var colors = ["white", "yellow", "red", "orange", "yellow", "blue", "purple", "pink", "brown", "gray"];
-	var colorcounter = 0;
-	shuffle(colors);
 	const canvas = document.getElementById("canvas");
 	const ctx = canvas.getContext("2d");
 	canvas.width  = canvas.offsetWidth;
@@ -380,91 +413,56 @@ function drawLines(){
 	let cnx;
 	try {
 		cnx = JSON.parse(connections);
-//		console.log(cnx);
 		for(var i=0; i<cnx.length; i++){
-			// all connections loop
-	//		console.log(cnx[i]);
-			for(var j=0; j<cnx[i].length; j++){
-				// each portal in a connection
-	//			console.log(cnx[i][j]);
-				var strokecolor = getcolor(colors, colorcounter);
-				var zoneid = cnx[i][j][0];
-				var portalid = cnx[i][j][1];
-				var zone_x = all_zones[zoneid].offsetLeft;
-				var zone_y = all_zones[zoneid].offsetTop;
-	//			console.log("ZONE: " + zone_x + ", " + zone_y)
-	//			console.log(all_zones[zoneid].childNodes);
-				var portal_x = all_zones[zoneid].childNodes[(portalid+1)].offsetLeft;
-				var portal_y = all_zones[zoneid].childNodes[(portalid+1)].offsetTop;
-	//			console.log("PORTAL: " + portal_x + ", " + portal_y);
-			
-				if(j===0){
-					// first portal in connection
-					x = (zone_x)+(portal_x);
-					y = (zone_y)+(portal_y);
-					color1 = all_zones[zoneid].dataset.color;
-//					console.log("LINES");
-//					console.log(all_zones[zoneid].dataset.color);
-				} else if(j===1) {
-					// second portal
-					color2 = all_zones[zoneid].dataset.color;
-					const this_w = Math.abs((x-(zone_x+portal_x)));
-					const this_h = Math.abs((y-(zone_y+portal_y)));
-/*
-					const angle = 90 * (Math.PI / 180);
-					const x2 = 300 * Math.cos(angle);
-					const y2 = 300 * Math.sin(angle);
-*/
-					const gradient = ctx.createLinearGradient(x, y, (zone_x+portal_x), (zone_y+portal_y));
-//					var gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
-//					gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
-/*
-					if(x>(zone_x+portal_x)){
-						c2 = color1;
-						c1 = color2;
-					} else {
-						c1 = color1;
-						c2 = color2;
+			// all connections loop		
+			if(map_mode === "mapview" || map_main_zone === cnx[i][0][0] || map_main_zone === cnx[i][1][0]){
+				for(var j=0; j<cnx[i].length; j++){
+					// each portal in a connection
+					var zoneid = cnx[i][j][0];
+					var portalid = cnx[i][j][1];
+					var zone_x = all_zones[zoneid].offsetLeft;
+					var zone_y = all_zones[zoneid].offsetTop;
+					var portal_x = all_zones[zoneid].childNodes[(portalid+1)].offsetLeft;
+					var portal_y = all_zones[zoneid].childNodes[(portalid+1)].offsetTop;
+				
+					if(j===0){
+						// first portal in connection
+						x = (zone_x)+(portal_x);
+						y = (zone_y)+(portal_y);
+						color1 = all_zones[zoneid].dataset.color;
+					} else if(j===1) {
+						// second portal
+						color2 = all_zones[zoneid].dataset.color;
+						const this_w = Math.abs((x-(zone_x+portal_x)));
+						const this_h = Math.abs((y-(zone_y+portal_y)));
+	
+						const gradient = ctx.createLinearGradient(x, y, (zone_x+portal_x), (zone_y+portal_y));
+						gradient.addColorStop("0", color1);
+						gradient.addColorStop("1.0", color2);
+						ctx.strokeStyle = gradient;
+						ctx.lineWidth = 3;
+						ctx.beginPath();
+						ctx.moveTo((zone_x)+(portal_x)+(10), (zone_y)+(portal_y)+(10));
+						ctx.lineTo(x+(10), y+(10));
+						ctx.stroke();
+						x=0;
+						y=0;
 					}
-*/
-					gradient.addColorStop("0", color1);
-//					gradient.addColorStop("0.5", "#111111");
-					gradient.addColorStop("1.0", color2);
-					//console.log(gradient);
-//					console.log(this_w);
-//					console.log(this_h);
-					ctx.strokeStyle = gradient;
-					ctx.lineWidth = 3;
-					ctx.beginPath();
-					ctx.moveTo((zone_x)+(portal_x)+(10), (zone_y)+(portal_y)+(10));
-					ctx.lineTo(x+(10), y+(10));
-					ctx.stroke();
-					x=0;
-					y=0;
+				}
+			}
+			
+			if(map_mode === "zoneview" && map_main_zone !== cnx[i][0][0] && map_main_zone !== cnx[i][1][0]){
+				for(var j=0; j<cnx[i].length; j++){
+					var zoneid = cnx[i][j][0];
+					var portalid = cnx[i][j][1];
+					// Do css/canvas semicircles
 				}
 			}
 			
 		}
 	} catch (e) {
 		console.log("local storage connections data not valid JSON");
-//		return console.error(e);
 	}
-}
-
-function getcolor(arr, counter){
-	if(counter >= arr.length){
-		counter = 0;
-	} else {
-		counter++;
-	}
-	return arr[counter];
-}
-
-function randomColor(){
-	var colors = ["white", "yellow", "red", "orange", "yellow", "blue", "purple", "pink", "brown", "gray"];
-	var num = colors.length;
-	var i = Math.floor(Math.random()*num);
-	return colors[i];
 }
 
 function clearLines(){
@@ -475,23 +473,57 @@ function clearLines(){
 
 // APP FEATURES
 
-function toggleLabels(e, comp){
-	if(comp.state.labels){
-		try {
-			comp.mapstatechange({
-				labels : false
-			});
-		} catch(err) {
-			console.log("togglebuttons: not a map");
+function toggleLabels(e, bool){
+	
+	toggleHidden(e);
+	
+	toggleClass(bool, "label");
+	useLabels = !bool;
+}
+
+function toggleItems(e, bool){
+	
+	toggleHidden(e);
+	
+	toggleClass(bool, "items");
+	useItems = !bool;
+}
+
+function toggleBosses(e, bool){
+	
+	toggleHidden(e);
+
+	toggleClass(bool, "bosses");
+	useBosses = !bool;
+}
+
+
+function toggleHidden(e){
+	var newbool = false;
+	for(var i=0; i<e.target.classList.length; i++){
+		if(e.target.classList[i] === "hidden"){
+			newbool = true;
 		}
-		
+	}
+	
+	if(newbool){
+		e.target.classList.remove("hidden");
 	} else {
-		try {
-			comp.mapstatechange({
-				labels : true
-			});
-		} catch(err) {
-			console.log("togglebuttons: not a map");
+		e.target.classList.add("hidden");
+	}
+}
+
+function toggleClass(bool, classname){
+	var items = document.getElementsByClassName(classname);
+	if(bool){
+		// Currently YES, turn them OFF
+		for(var i=0; i<items.length; i++){
+			items[i].classList.add("off");
+		}
+	} else {
+		// Currently NO, turn them ON
+		for(var i=0; i<items.length; i++){
+			items[i].classList.remove("off");
 		}
 	}
 }
@@ -526,8 +558,8 @@ function shuffle(array) {
 // LISTENERS
 
 window.addEventListener('contextmenu', function (e) {
-//  console.log('context menu disabled');
-  e.preventDefault();
+  console.log('context menu disabled');
+//  e.preventDefault();
 }, false);
 window.addEventListener("transitionstart", function (e) {
 	clearLines();
@@ -536,7 +568,6 @@ window.addEventListener("transitionend", function (e) {
 	drawLines();
 });
 window.addEventListener("resize", (event) => {
-	clearLines();
 	drawLines();
 });
 
